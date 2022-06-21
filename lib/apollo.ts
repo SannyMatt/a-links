7c84ac5ee6f05feab89409ae7a792b9051defb14
Sannikov Dmitriy <sannikov.matt@gmail.com>
2022-06-21 14:00:47 +0200

    Fixed gql connection

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { relayStylePagination } from "@apollo/client/utilities";
 
 const apolloClient = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: `${process.env.AUTH0_BASE_URL}/api/graphql`,
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
